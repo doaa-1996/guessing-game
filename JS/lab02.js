c5d6fc87371f882b5d85e55466add80ab7b7c62e
doaa-1996 <doaaobeidat1996@gmail.com>
2021-02-16 16:18:21 +0200

    lab03

--- JS/lab02.js
+++ JS/lab02.js
@@ -1,12 +1,13 @@
 'use strict';
-
+let score=0;
 alert('Welcome to my page ,I will ask you some questions please answer with yes or no ')
 let myCountry = prompt('Do you think i live in Jordan?');
 myCountry = myCountry.toLowerCase();
-console.log('Do you think i live in Jordan?' + ' ' + myCountry);
+console.log('Do you think i live in Jordan ?' + ' ' + myCountry);
 
 if (myCountry === 'yes' || myCountry === 'y') {
   alert('Great job, I live in Jordan');
+  score++;
 }
 else if (myCountry === 'no' || myCountry === 'n') {
   alert('No, thats wrong i live in Jordan');
@@ -23,6 +24,8 @@ if (myEducation === 'yes' || myEducation === 'y') {
 }
 else if (myEducation === 'no' || myEducation === 'n') {
   alert('Good, you know me very good , i am an engineer');
+  score++;
+
 }
 else {
   alert('you entered unvalid answer');
@@ -36,6 +39,8 @@ if (myAge === 'yes' || myAge === 'y') {
 }
 else if (myAge === 'no' || myAge === 'n') {
   alert('congrats, your answer is true');
+  score++;
+
 }
 else {
   alert('you entered unvalid answer');
@@ -46,6 +51,8 @@ myFavoriteFood = myFavoriteFood.toLowerCase();
 console.log('Is my favorite food pizza?' + ' ' + myFavoriteFood);
 if (myFavoriteFood === 'yes' || myFavoriteFood === 'y') {
   alert('Yes, It is delecious, Isn\'t it?');
+  score++;
+
 }
 else if (myFavoriteFood === 'no' || myFavoriteFood === 'n') {
   alert('No, your answer is wrong,I love pizza');
@@ -62,6 +69,8 @@ if (myFavoriteColor === 'yes' || myFavoriteColor === 'y') {
 }
 else if (myFavoriteColor === 'no' || myFavoriteColor === 'n') {
   alert('yes, blue is much better');
+  score++;
+
 }
 else {
   alert('you entered unvalid answer');
@@ -70,6 +79,85 @@ else {
 
 let userName = prompt('what is your name?');
 alert('welcome ' + userName);
+
+let i = 0;
+alert('we will play a game ,you should guess the secret number , you have four trials only');
+while (i < 4) {
+  let secretNumber = prompt('guess the secret number ');
+  console.log('guess the secret number' + ' ' +secretNumber);
+
+  if (secretNumber > '4') {
+    alert('Too high , try again !');
+    i++;
+  } else if (secretNumber < '4') {
+    alert('Too low !,try again');
+    i++;
+  } else if (secretNumber === '4') {
+    alert('congrats, you guessed the secret number');
+    score++;
+
+    break;
+  }
+}
+alert('The secret number is 4');
+
+
+
+
+let arr = ['yamin', 'roaa', 'dana', 'hala', 'myar', 'mohammad'];
+
+let correct = false;
+for (let k=0; k < 6; k++) {
+  let siplings = prompt('name one of my siplings)').toLowerCase();
+  console.log('name one of my siplings' + ' ' + siplings);
+
+  for (let d = 0; d < arr.length; d++) {
+    let array = arr[d];
+    if (siplings === array) {
+      alert(' correct ');
+      score++;
+      correct = true;
+      break;
+    }
+  }
+  if (correct === true) {
+    break;
+  } else {
+    alert(' wrong answer, try again ');
+  }
+}
+alert(' My siplings are :   roaa , yamin , dana , hala, mohammad , myar ');
+
+
+alert('your total score  out of 7 is'+ ' ' +score);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# guessing-game.eslintrc.json
-.eslintrc.json
+
+Guessing Game
+
+welcome to my webpage , we will play a game , are you ready ?
+
+
+we will ask you five questions , please answer with yes OR y OR no OR n.
+
+we will give you a score depending on your answers.
+
+
+
+
+
